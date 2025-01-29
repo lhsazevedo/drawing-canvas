dd38106eeb8586fa0ddd45c5efdeab15301a5d83
Lucas Azevedo <lhs_azevedo@hotmail.com>
2025-01-29 23:01:33 +0000

    Move Pusher config to environment variables

--- client/src/pusher.ts
+++ client/src/pusher.ts
@@ -1,12 +1,20 @@
 import Pusher from 'pusher-js'
 
-const pusher = new Pusher('app-key', {
-  cluster: 'cluster',
-  forceTLS: false,
-  wsHost: 'localhost',
-  wsPort: 6001,
-  httpHost: 'localhost',
-  httpPort: 6001,
+const {
+  VITE_PUSHER_APP_KEY,
+  VITE_PUSHER_HOST,
+  VITE_PUSHER_PORT,
+  VITE_PUSHER_CLUSTER,
+  VITE_PUSHER_FORCE_TLS,
+} = import.meta.env
+
+const pusher = new Pusher(VITE_PUSHER_APP_KEY, {
+  cluster: VITE_PUSHER_CLUSTER,
+  forceTLS: VITE_PUSHER_FORCE_TLS === 'true',
+  wsHost: VITE_PUSHER_HOST,
+  wsPort: VITE_PUSHER_PORT,
+  httpHost: VITE_PUSHER_HOST,
+  httpPort: VITE_PUSHER_PORT,
 })
 
 export { pusher }
