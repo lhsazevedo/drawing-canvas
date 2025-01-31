dd85bf6d9311b9ae832e9533baa851f997a8eb20
Lucas Azevedo <lhs_azevedo@hotmail.com>
2025-01-31 01:43:30 +0000

    Add id prop to TextInput

--- client/src/components/DcTextInput.vue
+++ client/src/components/DcTextInput.vue
@@ -3,6 +3,7 @@ const modelValue = defineModel()
 defineProps<{
   placeholder?: string
   label?: string
+  id?: string
   error?: string
 }>()
 </script>
@@ -10,12 +11,19 @@ defineProps<{
 <template>
   <div>
     <!-- TODO: Accessible label -->
-    <label v-if="label" class="block text-lg" :class="{ 'text-red-500': error }">
+    <label
+      v-if="label"
+      :for="id || undefined"
+      class="block text-lg"
+      :class="{ 'text-red-500': error }"
+    >
       {{ label }}
     </label>
     <input
       type="text"
+      :id="id || undefined"
       v-model="modelValue"
+      v-bind="$attrs"
       :placeholder="placeholder"
       class="w-full px-4 py-2 rounded-lg border-2 border-black"
       :class="{ 'border-red-500': error }"
